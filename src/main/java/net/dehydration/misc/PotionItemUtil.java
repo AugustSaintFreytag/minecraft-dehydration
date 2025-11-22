f7648032b1430c06c1795ed70bbb02110e97bc46
August Saint Freytag <me@augustfreytag.com>
2025-11-22 19:48:07 +0100

    Update item hydration and contamination logic

--- src/main/java/net/dehydration/misc/PotionItemUtil.java
+++ src/main/java/net/dehydration/misc/PotionItemUtil.java
@@ -7,21 +7,17 @@ import net.minecraft.potion.Potions;
 
 public class PotionItemUtil {
 
-	public static boolean isContaminatedPotionItemStack(ItemStack stack) {
+	public static boolean isBrewedPotionItemStack(ItemStack stack) {
 		Potion potion = PotionUtil.getPotion(stack);
-		return isContaminatedPotionItem(potion);
+		return isBrewedPotionItem(potion);
 	}
 
-	public static boolean isContaminatedPotionItem(Potion potion) {
-		if (potion == Potions.WATER || potion == Potions.AWKWARD || potion == Potions.THICK || potion == Potions.HARMING
-				|| potion == Potions.LONG_POISON || potion == Potions.LONG_SLOWNESS
-				|| potion == Potions.LONG_WEAKNESS || potion == Potions.MUNDANE || potion == Potions.POISON
-				|| potion == Potions.SLOWNESS || potion == Potions.STRONG_HARMING
-				|| potion == Potions.STRONG_POISON || potion == Potions.STRONG_SLOWNESS || potion == Potions.WEAKNESS) {
+	public static boolean isBrewedPotionItem(Potion potion) {
+		if (potion != Potions.EMPTY && potion != Potions.WATER) {
 			return true;
-		} else {
-			return false;
 		}
+
+		return false;
 	}
 
 }
